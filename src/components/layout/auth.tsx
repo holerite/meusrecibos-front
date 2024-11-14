730bf41f0664d834949b5ecefd454b3c89deb4ee
felps <luisfelipeamorim@hotmail.com>
2024-11-14 20:39:39 -0300

    feat(login): Aba de seleção

--- src/components/layout/auth.tsx
+++ src/components/layout/auth.tsx
@@ -1,28 +1,47 @@
 import { ReactNode } from "react";
 import { Card, CardContent, CardDescription, CardTitle } from "../ui/card";
 import { DarkModeToggle } from "./dark-mode-toggle";
+import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface IAuthLayoutProps {
-    children: ReactNode;
-    title: string;
-    description: string;
+	children: ReactNode;
+	title: string;
+	description: string;
 }
 
 export function AuthLayout({ children, title, description }: IAuthLayoutProps) {
-    return (
-        <div className="w-screen h-screen flex items-center justify-center relative">
-            <Card className="shadow-none border-none max-w-sm w-full bg-none mx-4">
-                <CardTitle className="text-xl">{title}</CardTitle>
-                <CardDescription>
-                    {description}
-                </CardDescription>
-                <CardContent className="p-0 mt-4">
-                    {children}
-                </CardContent>
-            </Card>
-            <div className="absolute top-4 right-4">
-                <DarkModeToggle />
-            </div>
-        </div>
-    );
+	const { pathname } = useLocation();
+	const navigate = useNavigate();
+
+	return (
+		<div className="w-screen h-screen flex items-center justify-center relative">
+			<div className="flex flex-col gap-4 max-w-sm w-full">
+				<Tabs
+					defaultValue={pathname}
+					onValueChange={(tab) => {
+						navigate(tab);
+					}}
+					className="w-full mx-4"
+				>
+					<TabsList className="w-full">
+						<TabsTrigger className="w-full" value="/">
+							Gestor
+						</TabsTrigger>
+						<TabsTrigger className="w-full" value="/employees">
+							Colaborador
+						</TabsTrigger>
+					</TabsList>
+				</Tabs>
+				<Card className="shadow-none border-none max-w-sm w-full bg-none mx-4">
+					<CardTitle className="text-xl">{title}</CardTitle>
+					<CardDescription>{description}</CardDescription>
+					<CardContent className="p-0 mt-4">{children}</CardContent>
+				</Card>
+			</div>
+			<div className="absolute top-4 right-4">
+				<DarkModeToggle />
+			</div>
+		</div>
+	);
 }
