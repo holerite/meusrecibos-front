cd8ea9968e56af4b6a5475446678b3f384615ddc
felps <luisfelipeamorim@hotmail.com>
2024-11-19 23:13:13 -0300

    fix(receipt): Ajuste texto modal de erro de importação

--- src/pages/receipt/import-receipt-error.modal.tsx
+++ src/pages/receipt/import-receipt-error.modal.tsx
@@ -1,66 +1,83 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
 } from "@/components/ui/dialog";
-import { Table, TableCell, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+	Table,
+	TableCell,
+	TableBody,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
 import { IImportReceiptErrorDialogValues } from "./receipt.page";
 
 interface IImportReceiptErrorDialogProps {
-    isOpen: boolean;
-    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-    errors: IImportReceiptErrorDialogValues[]
+	isOpen: boolean;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	errors: IImportReceiptErrorDialogValues[];
 }
 
-export function ImportReceiptErrorDialog({ isOpen, setIsOpen, errors }: IImportReceiptErrorDialogProps) {
+export function ImportReceiptErrorDialog({
+	isOpen,
+	setIsOpen,
+	errors,
+}: IImportReceiptErrorDialogProps) {
+	function handleCloseDialog() {
+		setIsOpen(false);
+	}
 
-    function handleCloseDialog() {
-        setIsOpen(false);
-    }
+	const totalErrors = errors.reduce(
+		(acc, error) => acc + error.pages.length,
+		0,
+	);
 
-    const totalErrors = errors.reduce((acc, error) => acc + error.pages.length, 0);
-
-    return (
-        <>
-            <Dialog
-                open={isOpen}
-                onOpenChange={setIsOpen}
-            >
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Erro ao importar recibos</DialogTitle>
-                    </DialogHeader>
-                    <div>
-                        <p>Algumas páginas não seguiram o padrão de estrutura do tipo de recibo selecionado. Por favor, verifique as páginas listadas abaixo.</p>
-                        <Badge className="mt-2" variant={"destructive"}>
-                            Número de páginas não importadas: {totalErrors}
-                        </Badge>
-                        <Table className="mt-2">
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>Nome do arquivo</TableHead>
-                                    <TableHead>Páginas</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {errors.map((error) => (
-                                    <TableRow key={error.name}>
-                                        <TableCell>{error.name}</TableCell>
-                                        <TableCell>{error.pages.join(", ")}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </div>
-                    <DialogFooter>
-                        <Button variant={"outline"} onClick={handleCloseDialog}>Fechar</Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
-        </>
-    );
+	return (
+		<>
+			<Dialog open={isOpen} onOpenChange={setIsOpen}>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Erro ao importar recibos</DialogTitle>
+					</DialogHeader>
+					<div>
+						<p>
+							Algumas páginas não seguiram o padrão de estrutura do tipo de
+							recibo selecionado. Por favor, verifique as páginas listadas
+							abaixo.
+						</p>
+						<p>As paginas corretas foram importadas normalmente.</p>
+						<Badge className="mt-2" variant={"destructive"}>
+							Número de páginas não importadas: {totalErrors}
+						</Badge>
+						<Table className="mt-2">
+							<TableHeader>
+								<TableRow>
+									<TableHead>Nome do arquivo</TableHead>
+									<TableHead>Páginas</TableHead>
+								</TableRow>
+							</TableHeader>
+							<TableBody>
+								{errors.map((error) => (
+									<TableRow key={error.name}>
+										<TableCell>{error.name}</TableCell>
+										<TableCell>{error.pages.join(", ")}</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</div>
+					<DialogFooter>
+						<Button variant={"outline"} onClick={handleCloseDialog}>
+							Fechar
+						</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</>
+	);
 }
